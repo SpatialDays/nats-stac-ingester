7a8d8a91bd7e1a8f21691e85e730b17904acdbe6
Ivica <ivica-matic@outlook.com>
2022-08-25 11:59:06 +0100

    Added ability to ingest both collections and items

--- src/stac_ingester/nats_consumer.py
+++ src/stac_ingester/nats_consumer.py
@@ -1,19 +1,27 @@
+import os
 import asyncio
 import logging
-from logging import INFO
 import signal
-LOG_FORMAT = '%(asctime)s - %(levelname)6s - %(message)s'
-LOG_LEVEL = INFO
+from logging import INFO
 
 from nats.aio.client import Client as NATS
 from stac_fastapi_ingest import ingest_catalog, ingest_collection, ingest_item
 
+LOG_FORMAT = '%(asctime)s - %(levelname)6s - %(message)s'
+LOG_LEVEL = INFO
+
 logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
 logger = logging.getLogger(__name__)
 
-NATS_SERVER_URL = "nats://localhost:4222"
+NATS_SERVER_URL = os.environ.get("NATS_STAC_INGESTER_NATS_SERVER_URL", "nats://localhost:4222")
 
-async def run(loop):
+
+async def run(loop: asyncio.AbstractEventLoop) -> None:
+    """
+    Run the NATS client.
+    :param loop: Asyncio event loop.
+    :return: None
+    """
     nc = NATS()
 
     async def closed_cb():
@@ -29,7 +37,7 @@ async def run(loop):
 
     await nc.connect(**options)
     logger.info(f"Connected to NATS at {nc.connected_url.netloc}...")
-    
+
     async def message_handler_ingest_catalog(msg):
         subject = msg.subject
         data = msg.data.decode()
@@ -41,20 +49,17 @@ async def run(loop):
         data = msg.data.decode()
         logger.info(f"Received a message on '{subject}'")
         ingest_collection(data)
-    
+
     async def message_handler_ingest_item(msg):
         subject = msg.subject
         data = msg.data.decode()
         logger.info(f"Received a message on '{subject}'")
         ingest_item(data)
-    
-
 
     # await nc.subscribe("nats_stac_ingester.*", cb=message_handler)
     await nc.subscribe("nats_stac_ingester.catalog", cb=message_handler_ingest_catalog)
     await nc.subscribe("nats_stac_ingester.collection", cb=message_handler_ingest_collection)
     await nc.subscribe("nats_stac_ingester.item", cb=message_handler_ingest_item)
-
 
     def signal_handler():
         if nc.is_closed:
--- src/stac_ingester/stac_fastapi_ingest.py
+++ src/stac_ingester/stac_fastapi_ingest.py
@@ -1,66 +1,65 @@
 import json.decoder
-from urllib.parse import urljoin
-import os
-import requests
 import logging
+import os
 from logging import INFO
+from urllib.parse import urljoin
+
+import requests
+
 LOG_FORMAT = '%(asctime)s - %(levelname)6s - %(message)s'
 LOG_LEVEL = INFO
-
 
 logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
 logger = logging.getLogger(__name__)
 
-STAC_SERVER_URL = os.environ.get(
-    "NATS_STAC_INGESTER_STAC_SERVER_URL", "http://localhost:8082/")
+STAC_SERVER_URL = os.environ.get("NATS_STAC_INGESTER_STAC_SERVER_URL", "http://localhost:8082/")
+STAC_SERVER_PROVIDER_NAME = os.environ.get("NATS_STAC_INGESTER_STAC_SERVER_PROVIDER_NAME", "")
+STAC_SERVER_PROVIDER_URL = os.environ.get("NATS_STAC_INGESTER_STAC_SERVER_PROVIDER_URL", "")
 
 
 def ingest_catalog(data):
-    print(data)
-    # catalog = get_json_from_url(url=catalog_url)
-
-    # if catalog.get('type') != 'Catalog':
-    #     raise RuntimeError(f"{catalog_url} is not a STAC Catalog.")
-
-    # logger.info("Ingesting catalog...")
-
-    # for link in catalog.get('links'):
-    #     if link.get('rel') == 'child':
-    #         ingest_collection(app_host=app_host, collection_url=link.get('href'))
+    pass
 
 
-def ingest_collection(data_as_string):
+def ingest_collection(data_as_string: str) -> None:
+    """
+    Ingest a collection into the STAC server.
+    :param data_as_string: Collection represented as a JSON string.
+    :return: None
+    :raise KeyError: If the link is not present in the list of links inside collection.
+
+    """
     data = json.loads(data_as_string)
-    # print(data)
-    links = data["links"]
-
-    RELS_THAT_NEED_REMOVING = ["items", "parent", "root", "self", "collection"]
-
-    # filter out the rels that we don't want to ingest
-    links = [link for link in links if link["rel"]
-             not in RELS_THAT_NEED_REMOVING]
-    # put the links back into the data
-    data["links"] = links
-    # print data in json format indented
-    # print(json.dumps(data, indent=4))
+    data["links"] = remove_unwanted_links(data["links"])
+    data["providers"] = add_our_provider_to_list_of_providers(data["providers"])
     post_or_put(urljoin(STAC_SERVER_URL, "collections"), data)
 
 
-def ingest_item(data_as_string):
+def ingest_item(data_as_string: str) -> None:
+    """
+    Ingest an item into the STAC server.
+
+    :param data_as_string: Item represented as a JSON string.
+    :return: None
+
+    :raise KeyError: If the collection reference is not present in the item.
+    :raise KeyError: If the link is not present in the list of links inside item.
+    """
     data = json.loads(data_as_string)
-    print(data)
-    # item = get_json_from_url(url=item_url)
-
-    # if item.get('type') != 'Feature':
-    #     raise RuntimeError(f"{item_url} is not a STAC Item.")
-
-    # logger.info(f"Ingesting {item.get('id')} item...")
-
-    # post_or_put(urljoin(app_host, f"collections/{item.get('collection')}/items"), item)
+    collection = data["collection"]
+    data["links"] = remove_unwanted_links(data["links"])
+    post_or_put(urljoin(STAC_SERVER_URL, f"collections/{collection}/items"), data)
 
 
 def post_or_put(url: str, data: dict):
-    """Post or put data to url."""
+    """
+    Post or put data to the given url.
+    :param url: Url to make the request to.
+    :param data: Data to use in body of request.
+    :return: None
+
+    :raise RuntimeError: If the request fails.
+    """
     try:
         r = requests.post(url, json=data)
         if r.status_code == 409:
@@ -73,3 +72,38 @@ def post_or_put(url: str, data: dict):
             r.raise_for_status()
     except requests.exceptions.RequestException as e:
         raise RuntimeError(f"Error while data ingesting: {e}")
+
+
+def remove_unwanted_links(links: list) -> list:
+    """
+    Remove unwanted links from a list of links.
+
+    Before ingesting items or collections into the STAC server, we need to remove the unwanted links that
+    point to the original data source so our stac-api can recreate them with appropriate links.
+
+    Args:
+    :param links: List of links to remove unwanted links from.
+    :return: List of links with unwanted links removed.
+    """
+    rels_that_need_removing = ["items", "parent", "root", "self", "collection"]
+    # filter out the rels that we don't want to ingest
+    links = [link for link in links if link["rel"]
+             not in rels_that_need_removing]
+    return links
+
+
+def add_our_provider_to_list_of_providers(providers: list) -> list:
+    """
+    Add our provider to the list of providers.
+
+    The provider and it's url are obtained from the environment variables called
+    NATS_STAC_INGESTER_STAC_SERVER_PROVIDER_NAME and
+    NATS_STAC_INGESTER_STAC_SERVER_PROVIDER_URL.
+
+    Args:
+    :param providers: List of providers.
+    :return: List of providers with our provider added.
+    """
+    if STAC_SERVER_PROVIDER_NAME and STAC_SERVER_PROVIDER_URL:
+        providers.append({"name": STAC_SERVER_PROVIDER_NAME, "url": STAC_SERVER_PROVIDER_URL, "roles": ["host"]})
+    return providers
